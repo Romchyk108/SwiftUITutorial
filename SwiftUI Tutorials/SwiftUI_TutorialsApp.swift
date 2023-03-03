c19616b19e7991838e0e1e833bb59d1b29253312
Roman Shestopal <romchyk.r@gmail.com>
2023-03-03 10:31:43 +0200

    Initial Commit

---
+++ SwiftUI Tutorials/SwiftUI_TutorialsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_TutorialsApp.swift
+//  SwiftUI Tutorials
+//
+//  Created by Roman Shestopal on 03.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_TutorialsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
